883fc8a47a47ac469360ce4d214562cb7f1c85de
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-01-24 03:47:10 -0800

    [Redux] update last message and styling

--- hellochat-web/src/components/SidebarChat.js
+++ hellochat-web/src/components/SidebarChat.js
@@ -7,6 +7,15 @@ import db from "../firebase"
 
 function SidebarChat({ addNewChat , id, name }) {
   const [seed, setSeed] = useState('');
+  const [messages, setMessages] = useState("");
+
+  useEffect(() => {
+    if (id) {
+      db.collection('rooms').doc(id).collection('messages').orderBy('timestamp', 'desc').onSnapshot(snapshot => (
+        setMessages(snapshot.docs.map((doc) => doc.data()))
+      ))
+    }
+  }, [])
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
@@ -31,7 +40,7 @@ function SidebarChat({ addNewChat , id, name }) {
         <Avatar src={`https://avatars.dicebear.com/4.5/api/human/${seed}.svg`} />
         <div className="sidebarChat__info">
           <h2>{name}</h2>
-          <p>Last message...</p>
+          <p>{messages[0]?.message}</p>
         </div>
       </div>
     </Link>
